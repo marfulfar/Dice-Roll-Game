c24432944495594555ffa95ffc8841297012867e
marfulfar <marcalfargas@gmail.com>
2023-04-25 11:39:09 +0200

    update player name

--- src/main/java/com/fargas/marcal/S5T2/repositories/MongoGameRepo.java
+++ src/main/java/com/fargas/marcal/S5T2/repositories/MongoGameRepo.java
@@ -3,5 +3,5 @@ package com.fargas.marcal.S5T2.repositories;
 import com.fargas.marcal.S5T2.entities.Game;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface MongoGameRepo extends MongoRepository<Game, Integer> {
+public interface MongoGameRepo extends MongoRepository<Game, String> {
 }
--- src/main/java/com/fargas/marcal/S5T2/repositories/MongoPlayerRepo.java
+++ src/main/java/com/fargas/marcal/S5T2/repositories/MongoPlayerRepo.java
@@ -3,5 +3,5 @@ package com.fargas.marcal.S5T2.repositories;
 import com.fargas.marcal.S5T2.entities.Player;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface MongoPlayerRepo extends MongoRepository<Player, Integer> {
+public interface MongoPlayerRepo extends MongoRepository<Player, String> {
 }
